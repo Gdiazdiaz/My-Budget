15b346720042149d942c51941dc6fc97e7e38efa
Glenda Diaz <glendadiazah@outlook.com>
2023-04-26 19:51:37 -0600

    Fix foreing keys

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,4 @@
 class Group < ApplicationRecord
     belongs_to :author, class_name: 'User'
+    has_many :movements, foreign_key: 'category_id'
 end
--- app/models/movement.rb
+++ app/models/movement.rb
@@ -1,3 +1,4 @@
 class Movement < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  belongs_to :category, class_name: 'Group'
 end
