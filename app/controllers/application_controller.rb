0e93bb2ece0358427c2145afc5537ae99f45da6f
Glenda Diaz <glendadiazah@outlook.com>
2023-04-29 04:28:21 -0600

    Fix visuals in aux links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
+  end
 
   def after_sign_in_path_for(resource)
     user_groups_path(resource)
