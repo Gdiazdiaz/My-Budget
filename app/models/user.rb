8d62faf8073097b81e9d0b23a085dc514341b130
Glenda Diaz <glendadiazah@outlook.com>
2023-05-24 20:20:44 -0600

    Remove confirmation by email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 
   has_many :movements, foreign_key: 'author_id'
   has_many :groups, foreign_key: 'author_id'
